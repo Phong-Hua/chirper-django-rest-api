8dda8065f9e6ad81738d85c247131d6d3e73b7d6
PhongHua <phonghua1989@hotmail.com>
2022-03-02 20:42:23 +1100

    Configure admin site

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
+
 from core import models
 
 
-admin.site.register(models.UserProfile)
+class UserAdmin(BaseUserAdmin):
+    ordering =['id']
+    list_display = ['email', 'name', 'avatarURL']
+
+    fieldsets = (
+        # None is title for section
+        (None, {
+            "fields": (
+                'email', 'password'
+            ),
+        }),
+        # if only one field, need to add coma
+        (_('Personal Info'), {'fields': ('name', 'avatarURL')}),
+        (
+            _('Permissions'),
+            {
+                'fields': ('is_active', 'is_staff', 'is_superuser')
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login', )})
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide', ),
+            'fields': ('email', 'name', 'password1', 'password2', 'avatarURL')
+        }),
+    )
+
+
+
+admin.site.register(models.UserProfile, UserAdmin)
---
+++ core/tests/test_admin.py
@@ -0,0 +1,60 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    # Setup before test
+    def setUp(self) -> None:
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email = 'admin@test.com',
+            password = 'password123',
+            name = 'admin',
+        )
+
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email = 'user1@test.com',
+            password = 'password123',
+            name = 'user1',
+            avatarURL = 'user1_avatar'
+        )
+
+    def test_users_listed(self):
+        """
+        Test that users are listed on user page.
+        """
+        # Refer to this document
+        # https://docs.djangoproject.com/en/2.1/ref/contrib/admin/#reversing-admin-urls
+        # for reverse admin
+        # core is the app name, userprofile is UserProfile model
+        url = reverse('admin:core_userprofile_changelist')
+        res = self.client.get(url)
+        # Check the http response 200
+        # And response contains certain items: name, email, avatarURL
+        self.assertEqual(res.status_code, 200)
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+        self.assertContains(res, self.user.avatarURL)
+
+    def test_user_change_page(self):
+        """
+        Test that user edit page works
+        """
+
+        url = reverse('admin:core_userprofile_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        # Test status code
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """
+        Test that create user page works
+        """
+        url = reverse('admin:core_userprofile_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
